320e35e51334eaff42739e539fa2aa2af534dd67
only4u <vuloc668165@gmail.com>
2026-05-29 10:46:11 +0700

    Update snake.h

--- src/snake.h
+++ src/snake.h
@@ -3,22 +3,18 @@
 
 #include <stdbool.h>
 
-typedef struct SnakeSegment {
-    int x, y;
-    struct SnakeSegment *next;
-} SnakeSegment;
+#define MAX_SNAKE_LEN 100
 
 typedef struct {
-    SnakeSegment *head;
-    SnakeSegment *tail;
+    int x[MAX_SNAKE_LEN];
+    int y[MAX_SNAKE_LEN];
     int length;
 } Snake;
 
-Snake* create_snake(int start_x, int start_y);
-void free_snake(Snake *snake);
+void init_snake(Snake *snake, int start_x, int start_y);
 void move_snake(Snake *snake, int dx, int dy);
-bool grow_snake(Snake *snake, int dx, int dy);
+void grow_snake(Snake *snake, int dx, int dy);
 bool check_self_collision(Snake *snake);
-void draw_snake(Snake *snake);
+void draw_snake(Snake *snake, int height, int width);
 
 #endif
